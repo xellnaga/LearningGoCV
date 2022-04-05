b25113251d294757a7c07907a0b3084f3ac0e097
woongbae.yoon <woongbae@gmail.com>
2022-04-06 00:32:35 +0900

    upload files

---
+++ 01.HelloLena/HelloLena.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+
+	"gocv.io/x/gocv"
+)
+
+func main() {
+	imageFilePath := "Lena.png"
+	mat := gocv.IMRead(imageFilePath, gocv.IMReadAnyColor)
+	if mat.Empty() {
+		log.Panic("Can not read Image file : ", imageFilePath)
+		return
+	}
+
+	log.Println("read", imageFilePath)
+	log.Println("size:", mat.Size())
+	log.Println("type:", mat.Type())
+	/*
+	mat = gocv.IMRead(imageFilePath, gocv.IMReadGrayScale)
+	if &mat == nil {
+		log.Panic("Can not read Image file : ", imageFilePath)
+		return
+	}
+	*/
+	log.Println("read", imageFilePath)
+	log.Println("size:", mat.Size())
+	log.Println("type:", mat.Type())
+	window := gocv.NewWindow("Lena")
+	window.IMShow(mat)
+	window.WaitKey(0)
+}
